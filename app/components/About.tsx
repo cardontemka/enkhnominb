eaad74e74e38bd95ac89c7f2d7ec6458a730ab20
cardontemka <ptemuulen82@gmail.com>
2026-01-10 20:55:09 +0800

    some cuteness

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,15 +1,23 @@
 import styles from './About.module.css'
-
+import Image from 'next/image'
 
 export default function About() {
     return (
         <section id="about" className={styles.about}>
             <div className={styles.container}>
-                <div className={styles.image}></div>
+                <div className={styles.imageWrap}>
+                    <Image
+                        src="/beautiful.jpg"
+                        alt="About image"
+                        fill
+                        className={styles.image}
+                        sizes="(max-width: 768px) 100vw, 50vw"
+                    />
+                </div>
                 <div className={styles.textContainer}>
                     <h2 className={styles.title}>About</h2>
                     <p className={styles.text}>
-                        About you. Beautiful, smart, film director etc. text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text 
+                        About you. Beautiful, smart, film director etc. text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text
                     </p>
                 </div>
             </div>
